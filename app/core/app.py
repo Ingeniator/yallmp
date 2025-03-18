e2e61ecde0d239f302f3c4161346094c3a4df5cf
MironovSergei <waityourletters@gmail.com>
2025-03-18 10:30:42 +0100

    feat(llm-authentification): add authorization header depends on selected authorization type

--- app/core/app.py
+++ app/core/app.py
@@ -1,9 +1,9 @@
 
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Depends, Request
 from app.core.config import settings
 from app.core.proxy import proxy_request_with_retries
 from app.core.logging_config import setup_logging
-# from app.services.authenticated_llm import get_authenticated_model
+from app.services.llm_authentication import get_authorization_headers
 from app.middlewares.logging_middleware import LoggingMiddleware
 from app.middlewares.metrics_middleware import PrometheusMiddleware, metrics
 
@@ -28,14 +28,7 @@ def create_app() -> FastAPI:
         return {"status": "Application is running"}
 
     @app.api_route("/llm/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"])
-    async def proxy_request(full_path: str, request: Request):
-        custom_headers = {}
-        if settings.llm_authorization_type == "BEARER":
-            custom_headers["Authorization"] = f"Bearer {settings.llm_api_key}"
-        elif settings.llm_authorization_type == "APIKEY":
-            custom_headers["X-API-Key"] = settings.llm_api_key
-        elif settings.llm_authorization_type == "CERT":
-            custom_headers["Authorization"] = f"Bearer {settings.llm_api_key}"
+    async def proxy_request(full_path: str, request: Request, custom_headers: dict[str, str] = Depends(get_authorization_headers)):
         return await proxy_request_with_retries(full_path, request, custom_headers)
 
     @app.on_event("startup")
